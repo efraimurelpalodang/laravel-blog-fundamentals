d0882be02e5c6135e6986435333a6ca7e616d832
efraim urel <efraimurelpalodang@gmail.com>
2026-02-26 21:03:09 +0800

    style: remove justify-align-start class from blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,9 +2,9 @@
 @section('title', 'Blog')
 @section('content')
     <h1>Halaman Posts</h1>
-    <div class="row gap-3 align-items-start mt-5">
+    <div class="row gap-3 mt-5">
         @foreach ($posts as $post)
-            <div class="card col-sm-4">
+            <div class="card col-sm-3">
                 <div class="card-header">
                     <h1 class="fs-5">
                         <a href="/post/{{ $post->id }}" class="text-black text-decoration-none">{{ $post->title }}</a>
